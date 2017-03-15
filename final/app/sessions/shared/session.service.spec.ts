f6e1e5e7ead7fe2ef9fe609b1fb391538495c7a9
Thomas Bandixen <thomas@bandixen.ch>
2017-03-15 17:30:06 +0100

    tests added

--- final/app/sessions/shared/session.service.spec.ts
+++ final/app/sessions/shared/session.service.spec.ts
@@ -1,3 +1,4 @@
+import { Session } from './session.model';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { async, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
 import { HttpModule, XHRBackend, Response, ResponseOptions } from "@angular/http";
@@ -20,14 +21,32 @@ describe("SessionService", () => {
         mockBackend = TestBed.get(XHRBackend);
     });
 
-    it("should get all sessions by observables", async(() => {
+    it("getById should return an instance of Session", async(() => {
         mockBackend.connections.subscribe((c: MockConnection) => {
-            expect(c.request.url).toContain("/api/sessions");
+            expect(c.request.url).toMatch(/\/api\/sessions\/42$/);
+
+            c.mockRespond(new Response(new ResponseOptions({
+                body: {
+                    data: { id: 42, title: "SessionTitle", room: "Zürich", rating: 4 }
+                }
+            })));
+        });
+
+        sessionService.getById(42).subscribe(session => {
+            mockBackend.verifyNoPendingRequests();
+            expect(session instanceof Session).toBeTruthy();
+            expect(session.title).toBe("SessionTitle");
+        });
+    }));
+
+    it("get should return instances of Session", async(() => {
+        mockBackend.connections.subscribe((c: MockConnection) => {
+            expect(c.request.url).toMatch(/\/api\/sessions$/);
 
             c.mockRespond(new Response(new ResponseOptions({
                 body: {
                     data: [
-                        { title: "SessionTitle" }
+                        { id: 42, title: "SessionTitle", room: "Zürich", rating: 4 }
                     ]
                 }
             })));
@@ -35,6 +54,7 @@ describe("SessionService", () => {
 
         sessionService.get().subscribe(sessions => {
             mockBackend.verifyNoPendingRequests();
+            expect(sessions[0] instanceof Session).toBeTruthy();
             expect(sessions[0].title).toBe("SessionTitle");
         });
     }));
