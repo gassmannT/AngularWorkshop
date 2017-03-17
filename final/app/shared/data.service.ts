518fa07119ae986af5c2b2204b240ea47950bc0e
Thomas Bandixen <thomas.bandixen@trivadis.com>
2017-03-17 10:36:43 +0100

    updated

--- final/app/shared/data.service.ts
+++ final/app/shared/data.service.ts
@@ -1,12 +1,12 @@
-import { InMemoryDbService } from "angular-in-memory-web-api";
-import { Session } from "../sessions/shared/session.model";
+import { Session } from '../sessions/shared/session.model';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 export class DataService implements InMemoryDbService {
     createDb() {
         let sessions: Session[] = [
-            new Session(1, "The ultimate Angular workshop", "Regensdorf", 4),
-            new Session(2, "Testing mit Angular", "Frankfurt", 4),
-            new Session(3, "Deployment von Angular Applikationen", "Nürnberg", 4)
+            new Session(1, "Testing mit Angular", "Frankfurt", 4),
+            new Session(2, "The ultimate Angular workshop", "Regensdorf", 5),
+            new Session(3, "Deployment von Angular Applikationen", "Nürnberg", 3)
         ];
         return { sessions };
     }
